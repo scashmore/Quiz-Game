9ce8b497ad31e9ffeeb2051a80b4c9087804bc62
Susan Ashmore <scashmorecitcom@gmail.com>
2021-02-16 23:04:51 -0500

    edited question array to array w/objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,41 @@
 //Array of question and answers
-var questoins = [
-    "What currecny is used in Animal Crossing?",
-    "Who will yell at you for not saving your game?",
-    "All of these modes of major transpertation appear in the Animal Crossing series except:",
-    "What type of animals sell you clothes and fancy accesories?",
-    "This cool dog is the game's top DJ.",
-    "Which character must you pay in order to own your house?"
+var questions= [{
+
+    question: "What currecny is used in Animal Crossing?",
+    choices: ["Bells", "Rupees", "Gold", "Coins"],
+    answer: "Bells"},
+{
+    question: "Who will yell at you for not saving your game?",
+    choices: ["Isabelle", "Harriet", "Resetti", "Blathers"],
+    answer: "Resetti"},
+{
+    question: "All of these modes of major transpertation appear in the Animal Crossing series except:",
+    choices: ["Plane", "Subway", "Train", "Boat"],
+    answer: "Subway"},
+{
+    question: "What type of animals sell you clothes and fancy accesories?",
+    choices: ["Skunks", "Hedgehogs", "Racoons", "Frogs"],
+    answer: "Hedgehogs"},
+{
+    question: "This cool dog is the game's top DJ.",
+    choices: ["K.K Slider", "Digby", "Goldie", "Booker"],
+    answer: "K.K Slider"},
+{
+    question: "Which character must you pay in order to own your house?",
+    choices: ["Luna", "Harvey", "Gracie", "Tom Nook"],
+    answer: "Tom Nook"}
 ];
-var answers = [
-    ["Bells", "Rupees", "Gold", "Coins"],
-    ["Isabelle", "Harriet", "Resetti", "Blathers"],
-    ["Plane", "Subway", "Train", "Boat"],
-    ["Skunks", "Hedgehogs", "Racoons", "Frogs"],
-    ["K.K Slider", "Digby", "Goldie", "Booker"],
-    ["Luna", "Harvey", "Gracie", "Tom Nook"]
-];
+// var answers = [
+//     ["Bells", "Rupees", "Gold", "Coins"],
+//     ["Isabelle", "Harriet", "Resetti", "Blathers"],
+//     ["Plane", "Subway", "Train", "Boat"],
+//     ["Skunks", "Hedgehogs", "Racoons", "Frogs"],
+//     ["K.K Slider", "Digby", "Goldie", "Booker"],
+//     ["Luna", "Harvey", "Gracie", "Tom Nook"]
+// ];
 
 // variable setup
-var time = 3;
+var time = 60;
 var timer = document.getElementById("timer");
 var container = document.getElementById("box1");
 var startQuiz = document.getElementById("start");
@@ -25,9 +43,9 @@ var resetQuiz = document.getElementById("reset");
 
 //functions
 function countDown(duration) {
-    setInterval(function() {
-        if(time >= 1) {
-            timer.textContent = "Time: " + time;
+    setInterval(function () {
+        if (time >= 1) {
+            timer.textContent = "Time: " + time + " seconds left";
             time--;
         }
         else {
@@ -36,4 +54,7 @@ function countDown(duration) {
     }, 1000);
 }
 
+
+
+//event listeners
 startQuiz.addEventListener("click", countDown);
